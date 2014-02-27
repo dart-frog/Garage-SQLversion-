642b7a062f2925d56324b52a606d866c0add1eca
Steve <Steve@Steve-DellPC>
2014-02-26 18:00:42 -0800

    Added changing state button

--- Garage(SQLversion)/src/Screen.java
+++ Garage(SQLversion)/src/Screen.java
@@ -1,13 +1,17 @@
 import java.awt.CardLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
 
 
 public class Screen extends JFrame {
 	
 	JPanel create;
 	JPanel home;
+	JPanel display;
 	JPanel masterPanel;
 	CardLayout cl;
 	public static void main(String[] args) {
@@ -23,8 +27,45 @@ public class Screen extends JFrame {
 		cl = new CardLayout();
 		masterPanel = new JPanel(cl);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setResizable(false);
+        setResizable(true);
         
         create = new JPanel();
+        
+        home = new JPanel();
+        display = new JPanel();
+        
+        masterPanel.add(home, "Home");
+        home.add(new JLabel("home"));
+        JButton addButton = new JButton("Add New Vehicle");
+        addButton.addActionListener( new SwitchStateButton(this, State.CREATE));
+        home.add(addButton);
+        masterPanel.add(create, "Create");
+        create.add(new JLabel("create"));
+        masterPanel.add(display, "Display");
+        display.add(new JLabel("display"));
+        
+        add(masterPanel);
+        
+	}
+	public void switchToCard(State state) {
+		if(masterPanel == null) return;
+		switch(state) {
+			case HOME:
+				((CardLayout) masterPanel.getLayout()).show(masterPanel, "Home");
+				break;
+			case CREATE:
+				((CardLayout) masterPanel.getLayout()).show(masterPanel, "Create");
+				break;
+			case DISPLAY:
+				((CardLayout) masterPanel.getLayout()).show(masterPanel, "Display");
+				break;
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+	public static enum State {
+		HOME,
+		CREATE,
+		DISPLAY
 	}
 }
---
+++ Garage(SQLversion)/src/SwitchStateButton.java
@@ -0,0 +1,18 @@
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SwitchStateButton  implements ActionListener{
+	Screen scr;
+	Screen.State state;
+	
+	public SwitchStateButton(Screen s, Screen.State state){
+		this.state = state;
+		Screen scr = s;
+	}
+	
+	public void actionPerformed(ActionEvent e) { 
+		scr.switchToCard(state); 
+	}
+}
